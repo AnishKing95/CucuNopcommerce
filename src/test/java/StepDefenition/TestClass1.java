2936ae72f6575d2461ff8e3dd951c785f4a33e5b
Newtech <Newtech@Newtech-PC>
2023-03-23 14:03:22 +0530

    commit from sudarsan

--- src/test/java/StepDefenition/TestClass1.java
+++ src/test/java/StepDefenition/TestClass1.java
@@ -3,7 +3,7 @@ package StepDefenition;
 public class TestClass1 {
 	
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("commit from sudarsan");
 		
 	}
 
