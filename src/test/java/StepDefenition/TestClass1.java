aeed637fb6b478803f4fdbf3c1872181611f410a
NEW <NEW@DELL>
2023-03-23 14:12:01 +0530

    submit by dina

--- src/test/java/StepDefenition/TestClass1.java
+++ src/test/java/StepDefenition/TestClass1.java
@@ -3,7 +3,7 @@ package StepDefenition;
 public class TestClass1 {
 	
 	public static void main(String[] args) {
-		System.out.println("commit from sudarsan");
+		System.out.println("new alert message");
 		
 	}
 
